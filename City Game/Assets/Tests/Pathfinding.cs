10e7d086358314ae75d2b61d1dc4506768fefdb7
EvanLF6768 <38551176+EvanLF6768@users.noreply.github.com>
2018-04-20 20:35:02 +1000

    Added Basic Roads Roads

---
+++ City Game/Assets/Tests/Pathfinding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    public NavMeshAgent obj;
+    RaycastHit info;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out info))
+        {
+            obj.SetDestination(info.point);
+        }
+	}
+}
