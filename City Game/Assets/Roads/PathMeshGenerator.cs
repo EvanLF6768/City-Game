3c79a900df2bb016940128d30943ef9e08f6a5f0
EvanLF6768 <38551176+EvanLF6768@users.noreply.github.com>
2018-04-22 09:48:51 +1000

    Completed PathMeshGenerator

--- City Game/Assets/Roads/PathMeshGenerator.cs
+++ City Game/Assets/Roads/PathMeshGenerator.cs
@@ -5,125 +5,150 @@ using System.Text;
 
 using UnityEngine;
 
-namespace Assets.Roads
+public static class PathMeshGenerator
 {
-    public static class PathMeshGenerator
-    {
-        private List<Vector2> m_points = new List<Vector2>();
+    const float resolution = 1f; // triangles per metre (Not that precise but it should do)
 
-        public PathMeshGenerator(Vector2[] points)
+    public static Mesh Generate(Spline path, float width)
+    {
+        int subdivisions = (int)Mathf.Ceil((path.GetValue(0) - path.GetValue(1)).magnitude * resolution);
+        float distancePerSample = 1f / subdivisions;
+
+        Vector3[] points = new Vector3[subdivisions * 2];
+
+        for (int i = 0; i < subdivisions; i++)
         {
-            m_points = new List<Vector2>(points);
+            float t = i * distancePerSample;
+            points[i] = path.GetValue(t) + path.GetNormalXZ(t).normalized * width;
         }
 
-        public int[] PathMeshGenerator()
+        for (int i = 0; i < subdivisions; i++)
         {
-            List<int> indices = new List<int>();
+            float t = i * distancePerSample;
+            points[i + subdivisions] = path.GetValue(t) - path.GetNormalXZ(t).normalized * width;
+        }
 
-            int n = m_points.Count;
-            if (n < 3)
+        int[] indices = Triangulate(points);
+        Vector3[] vertices = new Vector3[points.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(points[i].x, points[i].y, points[i].z);
+        }
+
+        Mesh mesh = new Mesh() { vertices = vertices, triangles = indices };
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        return mesh;
+    }
+
+    static int[] Triangulate(Vector3[] points)
+    {
+        List<Vector3>  m_points = new List<Vector3>(points);
+        List<int> indices = new List<int>();
+
+        int n = m_points.Count;
+        if (n < 3)
+            return indices.ToArray();
+
+        int[] V = new int[n];
+        if (Area(m_points) > 0)
+        {
+            for (int v = 0; v < n; v++)
+                V[v] = v;
+        }
+        else
+        {
+            for (int v = 0; v < n; v++)
+                V[v] = (n - 1) - v;
+        }
+
+        int nv = n;
+        int count = 2 * nv;
+        for (int m = 0, v = nv - 1; nv > 2;)
+        {
+            if ((count--) <= 0)
                 return indices.ToArray();
 
-            int[] V = new int[n];
-            if (Area() > 0)
+            int u = v;
+            if (nv <= u)
+                u = 0;
+            v = u + 1;
+            if (nv <= v)
+                v = 0;
+            int w = v + 1;
+            if (nv <= w)
+                w = 0;
+
+            if (Snip(u, v, w, nv, V, m_points))
             {
-                for (int v = 0; v < n; v++)
-                    V[v] = v;
+                int a, b, c, s, t;
+                a = V[u];
+                b = V[v];
+                c = V[w];
+                indices.Add(a);
+                indices.Add(b);
+                indices.Add(c);
+                m++;
+                for (s = v, t = v + 1; t < nv; s++, t++)
+                    V[s] = V[t];
+                nv--;
+                count = 2 * nv;
             }
-            else
-            {
-                for (int v = 0; v < n; v++)
-                    V[v] = (n - 1) - v;
-            }
-
-            int nv = n;
-            int count = 2 * nv;
-            for (int m = 0, v = nv - 1; nv > 2;)
-            {
-                if ((count--) <= 0)
-                    return indices.ToArray();
-
-                int u = v;
-                if (nv <= u)
-                    u = 0;
-                v = u + 1;
-                if (nv <= v)
-                    v = 0;
-                int w = v + 1;
-                if (nv <= w)
-                    w = 0;
-
-                if (Snip(u, v, w, nv, V))
-                {
-                    int a, b, c, s, t;
-                    a = V[u];
-                    b = V[v];
-                    c = V[w];
-                    indices.Add(a);
-                    indices.Add(b);
-                    indices.Add(c);
-                    m++;
-                    for (s = v, t = v + 1; t < nv; s++, t++)
-                        V[s] = V[t];
-                    nv--;
-                    count = 2 * nv;
-                }
-            }
-
-            indices.Reverse();
-            return indices.ToArray();
         }
 
-        private float Area()
-        {
-            int n = m_points.Count;
-            float A = 0.0f;
-            for (int p = n - 1, q = 0; q < n; p = q++)
-            {
-                Vector2 pval = m_points[p];
-                Vector2 qval = m_points[q];
-                A += pval.x * qval.y - qval.x * pval.y;
-            }
-            return (A * 0.5f);
-        }
-
-        private bool Snip(int u, int v, int w, int n, int[] V)
-        {
-            int p;
-            Vector2 A = m_points[V[u]];
-            Vector2 B = m_points[V[v]];
-            Vector2 C = m_points[V[w]];
-            if (Mathf.Epsilon > (((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))))
-                return false;
-            for (p = 0; p < n; p++)
-            {
-                if ((p == u) || (p == v) || (p == w))
-                    continue;
-                Vector2 P = m_points[V[p]];
-                if (InsideTriangle(A, B, C, P))
-                    return false;
-            }
-            return true;
-        }
-
-        private bool InsideTriangle(Vector2 A, Vector2 B, Vector2 C, Vector2 P)
-        {
-            float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
-            float cCROSSap, bCROSScp, aCROSSbp;
-
-            ax = C.x - B.x; ay = C.y - B.y;
-            bx = A.x - C.x; by = A.y - C.y;
-            cx = B.x - A.x; cy = B.y - A.y;
-            apx = P.x - A.x; apy = P.y - A.y;
-            bpx = P.x - B.x; bpy = P.y - B.y;
-            cpx = P.x - C.x; cpy = P.y - C.y;
-
-            aCROSSbp = ax * bpy - ay * bpx;
-            cCROSSap = cx * apy - cy * apx;
-            bCROSScp = bx * cpy - by * cpx;
-
-            return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
-        }
-
+        indices.Reverse();
+        return indices.ToArray();
     }
+
+    static float Area(List<Vector3> m_points)
+    {
+        int n = m_points.Count;
+        float A = 0.0f;
+        for (int p = n - 1, q = 0; q < n; p = q++)
+        {
+            Vector3 pval = m_points[p];
+            Vector3 qval = m_points[q];
+            A += pval.x * qval.z - qval.x * pval.z;
+        }
+        return (A * 0.5f);
+    }
+
+    static bool Snip(int u, int v, int w, int n, int[] V, List<Vector3> m_points)
+    {
+        int p;
+        Vector3 A = m_points[V[u]];
+        Vector3 B = m_points[V[v]];
+        Vector3 C = m_points[V[w]];
+        if (Mathf.Epsilon > (((B.x - A.x) * (C.z - A.z)) - ((B.z - A.z) * (C.x - A.x))))
+            return false;
+        for (p = 0; p < n; p++)
+        {
+            if ((p == u) || (p == v) || (p == w))
+                continue;
+            Vector2 P = m_points[V[p]];
+            if (InsideTriangle(A, B, C, P))
+                return false;
+        }
+        return true;
+    }
+
+    static bool InsideTriangle(Vector3 A, Vector3 B, Vector3 C, Vector3 P)
+    {
+        float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+        float cCROSSap, bCROSScp, aCROSSbp;
+
+        ax = C.x - B.x; ay = C.z - B.z;
+        bx = A.x - C.x; by = A.z - C.z;
+        cx = B.x - A.x; cy = B.z - A.z;
+        apx = P.x - A.x; apy = P.z - A.z;
+        bpx = P.x - B.x; bpy = P.z - B.z;
+        cpx = P.x - C.x; cpy = P.z - C.z;
+
+        aCROSSbp = ax * bpy - ay * bpx;
+        cCROSSap = cx * apy - cy * apx;
+        bCROSScp = bx * cpy - by * cpx;
+
+        return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
+    }
+
 }
---
+++ City Game/Assets/Roads/Spline.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+public struct Spline
+{
+    Parametic x, y, z;
+
+    public Spline(Vector3 startVal, Vector3 startDir, Vector3 endVal, Vector3 endDir)
+    {
+        x = new Parametic(startVal.x, startDir.x, endVal.x, endDir.x);
+        y = new Parametic(startVal.y, startDir.y, endVal.y, endDir.y);
+        z = new Parametic(startVal.x, startDir.x, endVal.x, endDir.x);
+    }
+
+    public Vector3 GetValue(float t)
+    {
+        return new Vector3(x.GetValue(t), y.GetValue(t), z.GetValue(t));
+    }
+
+    public Vector3 GetTangent(float t)
+    {
+        return new Vector3(x.GetTangent(t), y.GetTangent(t), z.GetTangent(t));
+    }
+
+    public Vector3 GetNormalXZ(float t)
+    {
+        return new Vector3(x.GetNormal(t), y.GetTangent(t), z.GetNormal(t));
+    }
+
+    public struct Parametic
+    {
+        float a, b, c, d;
+
+        public Parametic(float startVal, float startDir, float endVal, float endDir)
+        {
+            a = 2 * startVal + startDir + endDir - 2 * endVal;
+            b = 3 * endVal - 3 * startVal - 2 * startDir - endDir;
+            c = startDir;
+            d = startVal;
+        }
+
+        public float GetValue(float t)
+        {
+            return ((a * t + b) * t + c) * t + d;
+        }
+
+        public float GetTangent(float t)
+        {
+            return (3 * a * t + 2 * b) * t + c;
+        }
+
+        public float GetNormal(float t)
+        {
+            return -1f / GetTangent(t);
+        }
+    }
+}
